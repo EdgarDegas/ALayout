8d2955b565991f56ac32ec30e0f8268e90902822
iMoeNya <sym9688@outlook.com>
2018-12-20 18:42:20 +0800

    initialize

---
+++ ALayout/ALayout.h
@@ -0,0 +1,19 @@
+//
+//  ALayout.h
+//  ALayout
+//
+//  Created by 孙一萌 on 2018/12/20.
+//  Copyright © 2018 iMoe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ALayout.
+FOUNDATION_EXPORT double ALayoutVersionNumber;
+
+//! Project version string for ALayout.
+FOUNDATION_EXPORT const unsigned char ALayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ALayout/PublicHeader.h>
+
+
---
+++ ALayout/UIView+Layout.swift
@@ -0,0 +1,49 @@
+//
+//  UIView+GradientLayer.swift
+//  aCount
+//
+//  Created by 孙一萌 on 2018/11/24.
+//  Copyright © 2018 iMoe. All rights reserved.
+//
+
+import UIKit
+
+struct Shadow {
+    var color  : UIColor  = UIColor.black
+    var offset : CGVector = CGVector(dx: 0, dy: 0)
+    var opacity: CGFloat  = 0.15
+    var spread : CGFloat  = 0
+    var blur   : CGFloat  = 12
+    
+    init(color: UIColor, opacity: CGFloat, dx: CGFloat, dy: CGFloat, blur: CGFloat, spread: CGFloat) {
+        self.color = color; self.opacity = opacity; self.spread = spread; self.blur = blur
+        self.offset = CGVector(dx: dx, dy: dy)
+    }
+}
+
+extension UIView {
+    func addGradientLayer(gradientLayer: CAGradientLayer) {
+        gradientLayer.frame = bounds
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    func add(shadow: Shadow) -> CALayer {
+        let shadowLayer = CALayer()
+        shadowLayer.frame = bounds
+        shadowLayer.cornerRadius = layer.cornerRadius
+        shadowLayer.add(shadow: shadow)
+        shadowLayer.backgroundColor = backgroundColor?.cgColor
+        layer.insertSublayer(shadowLayer, at: 0)
+        return shadowLayer
+    }
+}
+
+extension CALayer {
+    func add(shadow: Shadow) {
+        shadowColor   = shadow.color.cgColor
+        shadowRadius  = shadow.blur
+        shadowOffset  = CGSize(width: shadow.offset.dx, height: shadow.offset.dy)
+        shadowOpacity = Float(shadow.opacity)
+        shadowPath = CGPath(roundedRect: bounds.insetBy(dx: -shadow.spread, dy: -shadow.spread), cornerWidth: cornerRadius, cornerHeight: cornerRadius, transform: nil)
+    }
+}
