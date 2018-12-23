e8bab48d6a6c092bafde26d955cd2b8fda2fa260
iMoeNya <sym9688@outlook.com>
2018-12-23 16:53:52 +0800

    Add init methods. Make variables editable.

--- ALayout/Shadow/Shadow.swift
+++ ALayout/Shadow/Shadow.swift
@@ -9,15 +9,21 @@
 import UIKit
 
 public struct Shadow {
-    var color  : UIColor  = UIColor.black
-    var offset : CGVector = CGVector(dx: 0, dy: 0)
-    var opacity: CGFloat  = 0.15
-    var spread : CGFloat  = 0
-    var blur   : CGFloat  = 12
+    public var color  : UIColor  = UIColor.black
+    public var offset : CGVector = CGVector(dx: 0, dy: 0)
+    public var opacity: CGFloat  = 0.15
+    public var spread : CGFloat  = 0
+    public var blur   : CGFloat  = 12
+    
+    public init() { }
     
     public init(color: UIColor, opacity: CGFloat, dx: CGFloat, dy: CGFloat, blur: CGFloat, spread: CGFloat) {
         self.color = color; self.opacity = opacity; self.spread = spread; self.blur = blur
         self.offset = CGVector(dx: dx, dy: dy)
+    }
+    
+    public init(color: UIColor, opacity: CGFloat, dx: CGFloat, dy: CGFloat, blur: CGFloat) {
+        self.init(color: color, opacity: opacity, dx: dx, dy: dy, blur: blur, spread: 0)
     }
 }
 
