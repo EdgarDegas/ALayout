64d5365431dbd715724cbde7aaeb238d9d69ec98
iMoeNya <sym9688@outlook.com>
2019-03-18 22:49:16 +0800

    add documentations

--- ALayout/Shadow/Shadow.swift
+++ ALayout/Shadow/Shadow.swift
@@ -8,16 +8,57 @@
 
 import UIKit
 
+/// An object that contains of shadow parameters.
+///
+/// You can set shadow for a UIView instance. See `set(shadow:)`
+/// of `UIView` for detail.
+///
+/// You can initialize a Shadow object using the same parameters from
+/// your app's design.
 public struct Shadow {
-    public typealias Offset = (dx: CGFloat, dy: CGFloat)
-    public var color  : UIColor  = .black
-    public var offset : CGVector = .init(dx: 0, dy: 0)
-    public var opacity: CGFloat  = 0
-    public var spread : CGFloat  = 0
-    public var blur   : CGFloat  = 0
     
+    /// Shadow offset.
+    ///
+    /// A positive value means rightwards on x-axis and downwards on y-axis.
+    public typealias Offset = (dx: CGFloat, dy: CGFloat)
+    
+    /// Color of the shadow.
+    public var color: UIColor  = .black
+    
+    /// The shadow's (x, y) offset. Defaults to (0, 0).
+    public var offset: CGVector = .init(dx: 0, dy: 0)
+    
+    /// Shadow opacity. Defaults to 0.
+    public var opacity: CGFloat  = 0
+    
+    /// Shadow's spreading size.
+    ///
+    /// You might be familiar with this property if you use Sketch often.
+    /// If not, this might be unnecessary for you.
+    ///
+    /// Setting a spread as 5 means the shadow path has (-5) insets on all four edges of
+    /// the layer.
+    ///
+    /// Anyway, larger the `spread`, larger the shadow is than the layer itself.
+    /// And this value can be negative.
+    public var spread: CGFloat  = 0
+    
+    /// Shadow blur radius.
+    public var blur: CGFloat  = 0
+    
+    /// Initialize a Shadow object with default shadow parameters.
     public init() { }
     
+    /// Initialize a Shadow object with the same parameters from your app's design.
+    /// - Parameters:
+    ///     - color: Color of the shadow. ShadowColor is a convenience setup for the basic
+    ///              UIColor and CGColor object. You can specify UIColor, ShadowRGBAColor
+    ///              or ShadowHexColor for this parameter.
+    ///     - opacity: Shadow opacity. Defaults to 0.
+    ///     - offset: Shadow offset. A positive value means rightwards on x-axis and downwards on y-axis.
+    ///     - blur: Shadow's blur radius.
+    ///     - spread: You might be familiar with this property if you use Sketch often.
+    ///               If not, this might be unnecessary for you. See `spread` for detail.
     public init(color: ShadowColor, opacity: CGFloat, offset: Offset, blur: CGFloat, spread: CGFloat) {
         self.color   = color.uiColor
         self.opacity = opacity
@@ -26,6 +67,14 @@ public struct Shadow {
         self.offset  = .init(dx: offset.dx, dy: offset.dy)
     }
     
+    /// Initialize a Shadow object with the same parameters from your app's design.
+    /// - Parameters:
+    ///     - color: Color of the shadow. ShadowColor is a convenience setup for the basic
+    ///              UIColor and CGColor object. You can specify UIColor, ShadowRGBAColor
+    ///              or ShadowHexColor for this parameter.
+    ///     - opacity: Shadow opacity. Defaults to 0.
+    ///     - offset: Shadow offset. A positive value means rightwards on x-axis and downwards on y-axis.
+    ///     - blur: Shadow's blur radius.
     public init(color: ShadowColor, opacity: CGFloat, offset: Offset, blur: CGFloat) {
         self.init(color: color, opacity: opacity, offset: offset, blur: blur, spread: 0)
     }
--- ALayout/Shadow/UIView+Shadow.swift
+++ ALayout/Shadow/UIView+Shadow.swift
@@ -10,14 +10,41 @@ import UIKit
 
 extension UIView: HavingShadow {
     
+    /// View's shadow layer.
+    ///
+    /// You can get its current shadow parameters by visiting shadowLayer's `shadow` property,
+    /// which is normally not nil if you **did not** directly instantiate the ShadowLayer.
     var shadowLayer: ShadowLayer? {
         return layer.sublayers?.first(where: { $0 is ShadowLayer }) as? ShadowLayer
     }
     
+    /// Remove the shadow layer from your view's layer hierarchy.
+    ///
+    /// - Parameters:
+    ///     - animated: If true, the shadow disappear with a fade transition.
+    ///     - completion: Block invoked after transition.
     open func removeShadow(animated: Bool = false, completion: (() -> Void)? = nil) {
         shadowLayer?.removeShadow(animated: animated, completion: completion)
     }
 
+    /// Add or change the shadow of the view.
+    ///
+    /// If you have not call this method before, this method creates a ShadowLayer object, which
+    /// is a CALayer subclass, and insert it at the bottom of the layer hierarchy of the view.
+    ///
+    /// If you've already set the shadow of the view, this method would update the shadow parameters
+    /// of the exsisting shadow layer instead of inserting a new one.
+    ///
+    /// You can visit view's `shadowLayer` property for current shadow parameters. This property is
+    /// nil if shadow has not been set.
+    ///
+    /// - Note:
+    ///     Shadow is a struct-based type, so you don't have to worry about any memory issues.
+    ///
+    /// - Parameters:
+    ///     - shadow: Shadow with parameters from you app's design.
+    ///     - animated: If true, the shadow changes with animation.
+    ///     - completion: Called when animation ends.
     open func set(shadow: Shadow, animated: Bool = false, completion: (() -> Void)? = nil) {
         if let shadowLayer = shadowLayer {
             shadowLayer.set(shadow: shadow, animated: animated, completion: completion)
@@ -26,6 +53,15 @@ extension UIView: HavingShadow {
         }
     }
     
+    /// This method reset the shadow layer's position and size.
+    ///
+    /// You should call this method in UIView or UIViewController's life cycle methods like
+    /// `viewDidLayoutSubviews` or `layoutSublayers(of:)`.
+    ///
+    /// Otherwise, the shadow layer's position or size would be wrong when the view
+    /// hierarchy is updated.
+    ///
+    /// This method will be deprecated once the layer behaviour is optimized.
     open func layoutShadow() {
         guard let sublayers = layer.sublayers else { return }
         for shadowLayer in sublayers where shadowLayer is ShadowLayer {
