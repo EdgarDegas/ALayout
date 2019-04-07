539457a8bf2c0e6bd08232bd45aee731fc6157b8
iMoe <iMoeNya@outlook.com>
2019-04-07 19:57:26 +0800

    add init parameter `distributeEvenly`
    If set distributeEvenly as true (which is the default value of the parameter), the locations of gradient layer is set even automatically. If false, the locations of the `GradientStop`s are used.

--- ALayout/Gradient/Model/GradientSettings.swift
+++ ALayout/Gradient/Model/GradientSettings.swift
@@ -11,10 +11,10 @@ import UIKit
 public struct GradientSettings {
     var colorStops: Array<GradientStop>
     
-    var beginPoint: Point
-    var endPoint  : Point
+    var beginPoint: Point!
+    var endPoint  : Point!
     
-    var direction: GradientLinearDirection {
+    var direction: GradientLinearDirection! {
         didSet {
             let points = direction.points
             beginPoint = points.begin
@@ -26,11 +26,25 @@ public struct GradientSettings {
         self = GradientSettings(colorStops: [begin, end])
     }
     
-    init(colorStops: Array<GradientStop>) {
+    init(colorStops: Array<GradientStop>, distributeEvenly: Bool = true) {
+        guard colorStops.count > 1 else {
+            self.colorStops = colorStops; return
+        }
+        
+        guard distributeEvenly else {
+            self.colorStops = colorStops; return
+        }
+        
+        self.colorStops = .init()
+        
+        let eachProgress = 1.0 / Double(colorStops.count - 1)
+        
+        for index in 0..<colorStops.count {
+            var stop = colorStops[index]
+            stop.progress = Double(index) * eachProgress
+            self.colorStops.append(stop)
+        }
+        
         self.colorStops = colorStops
-        direction = .vertical
-        let points = direction.points
-        beginPoint = points.begin
-        endPoint   = points.end
     }
 }
